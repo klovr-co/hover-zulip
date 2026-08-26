cb1e220e817e8aa86834ba21fda2612bac8414ec
Maxine Lai <ms.l@hotmail.com>
2026-08-26 20:16:54 +0800

    Remove Storybook integration

--- web/stories/story_utils.ts
+++
@@ -1,4 +0,0 @@
-export function component_story(content: string, vertical = false): string {
-    const direction = vertical ? " storybook-component-stack-vertical" : "";
-    return `<div class="storybook-component"><div class="storybook-component-stack${direction}">${content}</div></div>`;
-}
